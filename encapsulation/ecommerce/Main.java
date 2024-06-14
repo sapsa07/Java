0369d09b01ad5973c7501977acb98b4e158fc3c0
sapsa07 <sapsa007@gmail.com>
2024-06-14 21:46:41 +0530

    Non OOP Soln

--- encapsulation/ecommerce/Main.java
+++ encapsulation/ecommerce/Main.java
@@ -10,8 +10,22 @@ public class Main{
         androidHandler.change_user_country("Anand", "COUNTRY_USA");
         iosHandler.change_user_country("Sabina", "COUNTRY_INDIA");
 
-		androidHandler.change_user_language("Anand", "COUNTRY_USA", "LANGUAGE_SPANISH");
-        iosHandler.change_user_language("Sabina", "COUNTRY_INDIA", "LANGUAGE_HINDI");
-		androidHandler.change_user_language("Anupama", "COUNTRY_INDIA", "LANGUAGE_ENGLISH");
+		androidHandler.change_user_language(
+            "Anand", 
+            "COUNTRY_USA", 
+            "LANGUAGE_SPANISH"
+        );
+        
+        iosHandler.change_user_language(
+            "Sabina", 
+            "COUNTRY_INDIA", 
+            "LANGUAGE_HINDI"
+        );
+
+		androidHandler.change_user_language(
+            "Anupama", 
+            "COUNTRY_INDIA", 
+            "LANGUAGE_ENGLISH"
+        );
     }
 }
